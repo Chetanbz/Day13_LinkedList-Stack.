c5efd528bf64884d18e2cb7781d31bff09360c77
chetan <zambare.c123@gmail.com>
2020-12-03 18:54:16 +0530

    [Chetan] Added: Search Element

--- src/main/java/classsDay13/MyLinkedlist.java
+++ src/main/java/classsDay13/MyLinkedlist.java
@@ -44,6 +44,28 @@ public class MyLinkedlist {
         this.head = tempNode.getNext();
         return tempNode;
     }
+    public INode delete(int value){
+        INode deleteNode = null;
+        INode tempNode = this.head;
+        if(head.getKey().equals(value)) {
+            deleteNode = pop();
+        }
+        else if(tail.getKey().equals(value)){
+            deleteNode = popLast();
+        }
+        else {
+            while (!(tempNode.getNext() == null)) {
+                tempNode = tempNode.getNext();
+                if (tempNode.getNext().getKey().equals(value)) {
+                    deleteNode = tempNode.getNext();
+                    tempNode.setNext(tempNode.getNext().getNext());
+                }
+                tempNode = tempNode.getNext() ;
+            }
+        }
+        return  deleteNode;
+    }
+
     public INode popLast(){
         INode tempNode = head;
          while(!(tempNode.getNext() == this.tail)){
@@ -52,6 +74,31 @@ public class MyLinkedlist {
          this.tail = tempNode;
          tempNode = tempNode.getNext();
          return  tempNode;
+    }
+    public INode search(int value){
+        INode searchedNode = null;
+        INode tempNode = head;
+        if(this.tail.getKey().equals(value)) {
+            searchedNode = tail;
+            return searchedNode;
+        }
+        while(!(tempNode == this.tail)){
+            if (tempNode.getKey().equals(value)) {
+                searchedNode = tempNode;
+                break;
+            }
+            tempNode = tempNode.getNext() ;
+        }
+        return searchedNode;
+    }
+    public int size(){
+        int count = 1;
+        INode tempNode = head;
+        while(tempNode.getNext() != null){
+            count++;
+            tempNode = tempNode.getNext();
+        }
+        return  count;
     }
 
     public void printMyNode(){
@@ -65,4 +112,5 @@ public class MyLinkedlist {
         myNodes.append(tempNode.getKey());
         System.out.println(myNodes);
     }
+
 }
--- src/test/java/classsDay13/MyLinkedListTest.java
+++ src/test/java/classsDay13/MyLinkedListTest.java
@@ -1,9 +1,12 @@
 package classsDay13;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyLinkedListTest {
+
+
     @Test
     public void given3NumberAddedToLinkedListShouldNeAddedToTop() {
         MyNode<Integer> myFirstNode = new MyNode<>(70);
@@ -81,6 +84,74 @@ public class MyLinkedListTest {
                 myLinkedlist.tail.equals(mySecondNode);
         Assert.assertTrue(result);
     }
+    @Test
+    public void given3NumberAddedToLinkedListSearchElement() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedlist myLinkedlist = new MyLinkedlist();  // linkedList is made
+        myLinkedlist.append(myFirstNode);
+        myLinkedlist.append(mySecondNode);
+        myLinkedlist.append(myThirdNode);
+        INode searchedNode =  myLinkedlist.search(70);
+        myLinkedlist.printMyNode();
+        boolean result = searchedNode.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void given3NumberAddedToLinkedListSearchFails() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedlist myLinkedlist = new MyLinkedlist();  // linkedList is made
+        myLinkedlist.append(myFirstNode);
+        myLinkedlist.append(mySecondNode);
+        myLinkedlist.append(myThirdNode);
+        INode searchedNode =  myLinkedlist.search(90);
+        myLinkedlist.printMyNode();
+        boolean result = searchedNode == null;
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void given3NumberAddedToLinkedListSearchAndInsert() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyNode<Integer> myInsertedNode = new MyNode<>(40);
+        MyLinkedlist myLinkedlist = new MyLinkedlist();  // linkedList is made
+        myLinkedlist.append(myFirstNode);
+        myLinkedlist.append(mySecondNode);
+        myLinkedlist.append(myThirdNode);
+        INode searchedNode =  myLinkedlist.search(30);
+        myLinkedlist.insert(searchedNode,myInsertedNode);
+        myLinkedlist.printMyNode();
+        boolean result = myLinkedlist.head.equals(myFirstNode)  &&
+                myLinkedlist.head.getNext().equals(mySecondNode)&&
+                myLinkedlist.head.getNext().getNext().equals(myInsertedNode)&&
+                myLinkedlist.head.getNext().getNext().getNext().equals(myThirdNode)&&
+                myLinkedlist.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void given4NumberDeleteOneAndShowSize() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(40);
+        MyNode<Integer> myFourdNode = new MyNode<>(70);
+        MyLinkedlist myLinkedlist = new MyLinkedlist();  // linkedList is made
+        myLinkedlist.append(myFirstNode);
+        myLinkedlist.append(mySecondNode);
+        myLinkedlist.append(myThirdNode);
+        myLinkedlist.append(myFourdNode);
+        INode deleteNode = myLinkedlist.delete(40);
+        myLinkedlist.printMyNode();
+        int size = myLinkedlist.size();
+        boolean result = myLinkedlist.head.equals(myFirstNode)  &&
+                myLinkedlist.head.getNext().equals(mySecondNode)&&
+                myLinkedlist.head.getNext().getNext().equals(myFourdNode) && size==3;
+        Assert.assertTrue(result);
+    }
+
 
 }
 
