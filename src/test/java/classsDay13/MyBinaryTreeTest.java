36dd6292a4dee51ea9ddd318af32ff6276cb36c5
chetan <zambare.c123@gmail.com>
2020-12-04 16:54:50 +0530

    [Chetan] aded: BinaryThree Created for Three element

---
+++ src/test/java/classsDay13/MyBinaryTreeTest.java
@@ -0,0 +1,16 @@
+package classsDay13;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+    @Test
+    public void given3NumbersWhenAddedToBinaryTressShouldReturnSizeThree() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(3,size);
+    }
+}
